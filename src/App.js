1e72f41cd88ae93fc1cc9ba7b33872aaefa09b3c
ForgeGod <tommyrcao@gmail.com>
2023-10-31 22:10:26 -0400

    Lab 5 Fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>CS 230L</h1>
+            <h2>Section - 002</h2>
+            <p>WVU ID: 800358697</p>
+            <p>Hi I am Thomas Cao</p>
+        </div>
+    );
 }
 
 export default App;
