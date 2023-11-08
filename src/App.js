37a72ee689c1b2884cf6cbf811fd0027e45c2cac
ForgeGod <tommyrcao@gmail.com>
2023-11-07 22:20:53 -0500

    lab Seven update 3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 /* eslint-disable no-unused-vars */
 //import logo from './logo.svg';
 import './App.css';
-import navBar from './index';
-import card from './index';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Home';
 import contact from './Contact';
+import navBar from './NavBar';
+import card from './Card';
 
 function App() {
     return (
         <div className="App">
-     <Home></Home>
             <BrowserRouter>
-                <navbar />
+            <navBar />
                 <Routes>
                     <Route path="/" element={<Home />}></Route>
                     <Route path="/card" element={<card />}></Route>
--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.css";
@@ -7,7 +8,7 @@ function Home() {
     return (
         <div className="App">
             <navBar />
-            <h1>CS 230L</h1>git
+            <h1>CS 230L</h1>
             <h2>Section - 002</h2>
             <p>WVU ID: 800358697</p>
             <p>Hi I am Thomas Cao</p>
