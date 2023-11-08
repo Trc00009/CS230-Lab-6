03a2aa6c5acb4a1653844ccac2b8dce98dd17b88
ForgeGod <tommyrcao@gmail.com>
2023-11-07 22:10:24 -0500

    Lab Seven update 1

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.css";
-//import navBar from './NavBar';
+import navBar from './NavBar';
 //import card from './Card';
 function Home() {
     return (
         <div className="App">
+            <navBar />
             <h1>CS 230L</h1>git
             <h2>Section - 002</h2>
             <p>WVU ID: 800358697</p>
